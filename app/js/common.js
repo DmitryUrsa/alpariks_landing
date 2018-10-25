128a88493e2262d1057295aa975a3b8b893ec6aa
DmitryUrsa <r2d2btw@gmail.com>
2018-10-25 16:31:15 +0800

    Text block and slider

--- app/js/common.js
+++ app/js/common.js
@@ -11,4 +11,30 @@ $(function() {
 
 	});
 
+	jQuery('#works-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+            }
+        }, {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+
 });
